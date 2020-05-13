dde2bf56d4046a9e60143e85e5ac26fc774740da
sergei_vydra <vydrasergei@gmail.com>
2020-05-14 01:48:42 +0300

    added venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sergei_vydra/Work/PycharmProjects/study_projects/STEPIK/web-develop_django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

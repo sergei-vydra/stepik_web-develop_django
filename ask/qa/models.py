5357eb4d36d255572b2c53d3b9ab82f2e2c098f9
sergei_vydra <vydrasergei@gmail.com>
2020-05-15 02:06:32 +0300

    added qa.models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,7 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
@@ -16,11 +25,3 @@ class Answer(models.Model):
     added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
